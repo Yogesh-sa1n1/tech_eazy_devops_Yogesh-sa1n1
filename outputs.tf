7d8106fc10a6d8d6753fabc2ca17ccf38b32cb99
Yogesh <ys395908@gamil.com>
2025-07-11 00:31:31 +0530

    changes

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "key_pair_name" {
   value = aws_key_pair.generated_key.key_name
 }
 
-output "s3_bucket_name" {
+output "s3_bucket_name"{
   value       = aws_s3_bucket.log_bucket.bucket
   description = "Name of log S3 bucket"
 }
